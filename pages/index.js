5d08df860e4d4eddba2fd034ff0d92ffd9fc7fd3
Eva Soler Weinmann <evasolerweinmann@MacBook-Pro-de-Eva.local>
2022-04-12 23:49:27 +0200

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -10,19 +12,35 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Welcome to the Kinesiology Practice of <Link href="/coto">
+          <a>Carlos Coto</a>
+        </Link>.</p>
+        <h2>βαθύς</h2>
+        <p>Bathus is a Greek word that means: DEEP.
+        </p>
+        <p>Deep describes what we may do here for you: </p>
+         <p> 
+          <br />
+          <li>Kinesiology (Applied Physiology, Sport Kinesiology, Touch For Health)</li>
+          <li>Psychology Counseling (Anxiety Disorders, Eating Disorders, Psychooncology)</li> 
+          <li>NLP</li>
+          <li>Ericksonian Hypnosis</li> 
+          <li>Biomagnetism by Dr. Goiz</li>
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
